63e233f0e86ff26acc93a6b3302c9b99f54141fc
Hussein Amro <haa193@mail.aub.edu>
2023-11-24 23:28:08 +0200

    Project Done

--- script.js
+++ script.js
@@ -59,10 +59,6 @@ function loadHistory(historyBlock) {
     document.getElementById("key").value = key;
     document.getElementById("inputText").value = text;
     document.getElementById("option").value = option;
-    if ((window.innerWidth < 1200) && (window.innerHeight < 1200)) {
-        var slideModal = document.getElementById("menu");
-        slideModal.style.left = "-200%";
-    }
     changeInputLabel();
 }
 
